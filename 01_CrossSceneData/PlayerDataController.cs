fcc49acf30722d53f309bf745b08b7c751f06d2b
Yasudabo <46628648+yasudabo@users.noreply.github.com>
2020-12-18 01:27:27 +0800

    Add  Template - Cross-Scene Data
    跨場景數據傳遞範例

---
+++ 01_CrossSceneData/PlayerDataController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDataController : MonoBehaviour {
+
+    //死亡計數器
+    public soDeadCounter DeadCounter = null;
+
+    //單例
+    public static PlayerDataController Instance = null;
+    private void Awake() {
+        if (PlayerDataController.Instance == null) {
+            PlayerDataController.Instance = this;
+            DontDestroyOnLoad(this);
+            DeadCounter.f_Reset();
+        }
+        else {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            SceneManager.LoadScene("TestScene1");
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            SceneManager.LoadScene("TestScene2");
+        }
+        if (Input.GetKeyDown(KeyCode.B)) {
+            DeadCounter.DeadCount++;
+        }
+    }
+}
---
+++ 01_CrossSceneData/soDeadCounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Data", menuName = "ScriptableObject/Create 死亡計數器", order = 1)]
+/// <summary> 死亡計數器 </summary>
+public class soDeadCounter : ScriptableObject {
+
+    /// <summary> 死亡次數 </summary>
+    public int DeadCount = 0;
+
+    /// <summary> 重置 </summary>
+    public void f_Reset() {
+        DeadCount = 0;
+    }
+}
